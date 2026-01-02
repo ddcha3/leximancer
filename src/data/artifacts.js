ddf66c5409304e4093754f324ffe7077cdcd76a8
vsporeddy <vs.poreddy@gmail.com>
2026-01-01 21:42:49 -0800

    rat item?

--- src/data/artifacts.js
+++ src/data/artifacts.js
@@ -1,32 +1,37 @@
 // Permanent player artifacts (upgrades)
-// These are data-first artifacts; acquisition logic will be added later.
 export const ARTIFACTS = [
   {
     id: 'tomato',
     emoji: '🍅',
-    name: 'Tomato of Vitality',
-    desc: '+20 max HP. (Permanent)',
+    name: 'Tomato',
+    desc: '+20 max HP.',
     maxHpBonus: 20
   },
   {
     id: 'helmet',
     emoji: '🪖',
     name: 'Worn Helmet',
-    desc: 'Block 1 damage from all enemy spells. (Permanent)',
+    desc: 'Block 1 damage from all enemy spells.',
     incomingDamageBlock: 1
   },
   {
     id: 'crystal_ball',
     emoji: '🔮',
     name: 'Crystal Ball',
-    desc: 'Reveals enemy weaknesses in battle. (Permanent)',
+    desc: 'Reveals enemy weaknesses in battle.',
     revealWeaknesses: true
   },
   {
     id: 'fairy_wings',
     emoji: '🪽',
     name: 'Fairy Wings',
-    desc: 'Verbs deal +1 damage. (Permanent)',
+    desc: 'Verbs deal +1 damage.',
     verbBonusDamage: 1
+  },
+  {
+    id: 'rat',
+    emoji: '🐀',
+    name: 'Rat',
+    desc: "It won't stop following you.",
   }
 ];
